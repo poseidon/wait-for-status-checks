6f2a45f197c37c92a6b4d7b9b7e013f4763b5d5d
BJ Hargrave <bj@hargrave.dev>
2024-10-01 14:05:17 -0400

    pickSHA: Improve pickSHA to handle pull_request_target and push
    To support use in pull_request_target target workflows, we need to
    use the pull_request.head.sha value for check-runs.
    
    We also better support push target workflows by using the after
    SHA value as it better represents "The SHA of the most recent commit on
    ref after the push."
    
    Signed-off-by: BJ Hargrave <bj@hargrave.dev>

--- src/main.ts
+++ src/main.ts
@@ -46,10 +46,15 @@ async function run(): Promise<void> {
 }
 
 function pickSHA(context: Context): string {
-  if (context.eventName === 'pull_request') {
-    return context.payload.pull_request?.head.sha || context.sha
+  switch (context.eventName) {
+    case 'pull_request':
+    case 'pull_request_target':
+      return context.payload.pull_request?.head.sha || context.sha
+    case 'push':
+      return context.payload.after || context.sha
+    default:
+      return context.sha
   }
-  return context.sha
 }
 
 run()
