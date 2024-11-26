411890f361b58973c135894640e372190d68fa32
Maarten Groeneweg <maartengroeneweg@funda.nl>
2024-11-26 09:04:21 +0100

    feat: include neutral condition

--- src/poll.ts
+++ src/poll.ts
@@ -156,7 +156,7 @@ export async function poll(config: Config): Promise<void> {
 function isFailure(run: CheckRun): boolean {
   if (run.status === 'completed') {
     // all conclusions besides success or skipped are considered failures
-    return run.conclusion !== 'success' && run.conclusion !== 'skipped'
+    return run.conclusion !== 'success' && run.conclusion !== 'skipped' && run.conclusion !== 'neutral'
   }
   // run is still queued or pending
   return false
