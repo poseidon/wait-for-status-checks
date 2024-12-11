7c1775590c213dcdfc8f97828c17d2e435747591
Maarten Groeneweg <maartengroeneweg@funda.nl>
2024-12-11 10:40:13 +0100

    chore: run buildscripts

--- src/poll.ts
+++ src/poll.ts
@@ -156,7 +156,11 @@ export async function poll(config: Config): Promise<void> {
 function isFailure(run: CheckRun): boolean {
   if (run.status === 'completed') {
     // all conclusions besides success or skipped are considered failures
-    return run.conclusion !== 'success' && run.conclusion !== 'skipped' && run.conclusion !== 'neutral'
+    return (
+      run.conclusion !== 'success' &&
+      run.conclusion !== 'skipped' &&
+      run.conclusion !== 'neutral'
+    )
   }
   // run is still queued or pending
   return false
